a4f70d936713b1545785946152d64589f35f439f
ulisses gomes <ulissestark@gmail.com>
2022-05-04 15:43:25 -0300

    montando as primeiras functions em js

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+function showCamisas(){
+    document.getElementById("camisas").style.display="block";
+    document.getElementById("btHide").style.display="block";
+  }
+  function hideCamisas(){
+    document.getElementById("camisas").style.display="none";
+    document.getElementById("btHide").style.display="none";
+  }
+
+  
+  function bottons(){
+    document.getElementById("bottons").style.display="block";
+  }
+  function colecionaveis(){
+    document.getElementById("colecionaveis").style.display="block";
+  }
+  function hideProd(){
+    document.getElementById("produtos").style.display="none";
+  }
