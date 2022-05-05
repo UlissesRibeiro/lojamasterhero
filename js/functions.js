16de46d852e0c61b9d5180201b26cedf4287bb12
ulisses gomes <ulissestark@gmail.com>
2022-05-04 23:13:13 -0300

    create collapse buttons

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,29 @@
 function showCamisas(){
     document.getElementById("camisas").style.display="block";
-    document.getElementById("btHide").style.display="block";
+    document.getElementById("hideCamisa").style.display="block";
   }
   function hideCamisas(){
     document.getElementById("camisas").style.display="none";
-    document.getElementById("btHide").style.display="none";
+    document.getElementById("hideCamisa").style.display="none";
   }
 
   
-  function bottons(){
+  function showBottons(){
     document.getElementById("bottons").style.display="block";
+    document.getElementById("hideBotton").style.display="block";
   }
-  function colecionaveis(){
+  function hideBottons(){
+    document.getElementById("bottons").style.display="none";
+    document.getElementById("hideBotton").style.display="none";
+  }
+
+  function showCole(){
     document.getElementById("colecionaveis").style.display="block";
+    document.getElementById("hideColecionavel").style.display="block"
+  }
+  function hideCole(){
+    document.getElementById("colecionaveis").style.display="none";
+    document.getElementById("hideColecionavel").style.display="none"
   }
   function hideProd(){
     document.getElementById("produtos").style.display="none";
